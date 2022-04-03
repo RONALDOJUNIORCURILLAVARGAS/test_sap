63bef04f6f3512f523aba8d16d738aefb3fd78a9
RONALDOJUNIORCURILLAVARGAS <ronaldocurillavargas@gmail.com>
2022-04-02 19:49:47 -0500

    first

--- sap/settings.py
+++ sap/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-uys3sjb3r3gydphi@#5llp!5z7nhi!=3*^rnux6enia9he0imx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -102,7 +103,7 @@ WSGI_APPLICATION = 'sap.wsgi.application'
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 } """
-import dj_database_url
+""" import dj_database_url """
 DATABASES ={
     'default':{
         'ENGINE':'django.db.backends.postgresql_psycopg2',
@@ -114,12 +115,12 @@ DATABASES ={
     }
 }
 
-from decouple import config
+""" from decouple import config
 DATABASES={
     'default': dj_database_url.config(
         default=config('DATABASE_URL')
     )
-}
+} """
 """ db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env) """
 
@@ -159,7 +160,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+""" STATIC_URL = 'static/' """
+
+STATIC_ROOT =os.path.join(BASE_DIR,'staticfiles')
+STATIC_URL= '/static/'
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
